8f68de98f322e42993866e28acdee819585e88b1
Neharika Shimpiger <104868833+Tigerwoods0001@users.noreply.github.com>
2023-10-11 02:28:19 +1100

    Add files via upload
    Lab 10 Homework

---
+++ vega.js
@@ -0,0 +1,6 @@
+var vg_1 = "Visualisation.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+ }).catch(console.error);
+var vg_2 = "visualisation2.json";
+vegaEmbed("#line_chart", vg_2).then(function(result) {
+ }).catch(console.error);
